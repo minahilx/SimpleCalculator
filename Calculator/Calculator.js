50810d4d48e7d4e3c162d7f979368c54546f1b66
Minahil Babar <71601253+minahilx@users.noreply.github.com>
2023-07-04 09:56:11 +0500

    Add files via upload

---
+++ Calculator/Calculator.js
@@ -0,0 +1,17 @@
+class Calculator {
+    add(num1, num2) {
+        return num1 + num2; // Addition operation
+    }
+
+    subtract(num1, num2) {
+        return num1 - num2; // Subtraction operation
+    }
+
+    divide(num1, num2) {
+        return num1 / num2; // Division operation
+    }
+
+    multiply(num1, num2) {
+        return num1 * num2; // Multiplication operation
+    }
+}
